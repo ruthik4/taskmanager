f3311d1e68e489085fbe83bd0eb66463c11b6710
Ruthik4 <ruthikrakonde4@gmail.com>
2026-05-07 14:47:00 +0530

    chore: Prepare frontend for Railway deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://taskmanager-production-76f3.up.railway.app";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://taskmanager-production-76f3.up.railway.app";
+// export const API_URL = "http://localhost:8080";
